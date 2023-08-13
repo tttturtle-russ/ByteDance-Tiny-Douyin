40be41a7bd67c89de56f4e5d27801af2c91ab885
whale-withme <3052748978@qqcom>
2023-08-13 11:12:00 +0800

    action修改

--- favourite/action.go
+++ favourite/action.go
@@ -66,45 +66,7 @@ func Action(c *gin.Context) {
 	}
 
 	if userlike == true && req.ActionType == DislikeYou {
-		//删除取消点赞的id
-		if err := dao.Delete(userid, videoid); err != nil {
-			log.Printf("删除点赞id错误，%v", err)
-			res := &ActionResponse{
-				Code: 1,
-				Msg:  err.Error(),
-			}
-			c.JSON(http.StatusBadRequest, res)
-		}
-
-		//减少video总点赞数
-		if err := dao.TotalDown(videoid); err != nil {
-			log.Printf("减少视频点赞总数错误，%v", err)
-			res := &ActionResponse{
-				Code: 1,
-				Msg:  err.Error(),
-			}
-			c.JSON(http.StatusBadRequest, res)
-		}
-
-		//减少作者获赞数
-		if err := dao.AuthorDown(videoid); err != nil {
-			log.Printf("减少作者点赞总数错误，%v", err)
-			res := &ActionResponse{
-				Code: 1,
-				Msg:  err.Error(),
-			}
-			c.JSON(http.StatusBadRequest, res)
-		}
-
-		//减少用户点赞数
-		if err := dao.UserDown(userid); err != nil {
-			log.Printf("减少用户点赞错误， %v", err)
-			res := &ActionResponse{
-				Code: 1,
-				Msg:  err.Error(),
-			}
-			c.JSON(http.StatusBadRequest, res)
-		}
+		DisLike(userid, videoid, c)
 	}
 
 	//is_favourite处理（不确定是不是这么处理）
