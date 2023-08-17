21c5b5ab2083c26631a362c906a13c32d5d51d83
Criticayon <140525237+Criticayon@users.noreply.github.com>
2023-08-17 11:31:28 +0800

    Update comment.go
    add the function that can show the comment list

--- controller/comment.go
+++ controller/comment.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"ByteDance-Tiny-Douyin/model"
 	"ByteDance-Tiny-Douyin/service"
 	"ByteDance-Tiny-Douyin/util"
 	"github.com/gin-gonic/gin"
@@ -15,8 +16,12 @@ type CommentRequest struct {
 	CommentText string
 	CommentID   string
 }
+type CommentResponse struct {
+	CommentList []model.Comment
+	Response
+}
 
-func commentAction(c *gin.Context) {
+func CommentAction(c *gin.Context) {
 	var req CommentRequest
 	err := c.ShouldBindQuery(&req)
 	if err != nil {
@@ -36,7 +41,7 @@ func commentAction(c *gin.Context) {
 		})
 	}
 	svc := service.NewService(c)
-	status, err := svc.Comment(req)
+	status, err := svc.CommentAction(req)
 	if err != nil {
 		log.Println(err)
 		c.JSON(http.StatusBadRequest, Response{
@@ -49,4 +54,43 @@ func commentAction(c *gin.Context) {
 		StatusCode:    200,
 		StatusMessage: status,
 	})
+}
+
+func ListGet(c *gin.Context) {
+	var req CommentRequest
+	err := c.ShouldBindQuery(&req)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, Response{
+			StatusCode:    400,
+			StatusMessage: "参数错误",
+		})
+		return
+	}
+	_, err = util.ParseToken(req.Token)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusUnauthorized, Response{
+			StatusCode:    401,
+			StatusMessage: "用户未认证",
+		})
+	}
+	svc := service.NewService(c)
+	status, err := svc.CommentList(req)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, Response{
+			StatusCode:    400,
+			StatusMessage: "显示评论失败",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, CommentResponse{
+		Response: Response{
+			StatusCode:    200,
+			StatusMessage: "显示评论成功",
+		},
+		CommentList: status,
+	})
+
 }
