bd256a3bd83358724807c602845f2fb67bcf443d
Criticayon <140525237+Criticayon@users.noreply.github.com>
2023-08-17 11:34:43 +0800

    Update comment.go
    Add the function about showing comment list

--- service/comment.go
+++ service/comment.go
@@ -1,10 +1,17 @@
 package service
 
-import "ByteDance-Tiny-Douyin/controller"
+import (
+	"ByteDance-Tiny-Douyin/controller"
+	"ByteDance-Tiny-Douyin/model"
+)
 
-func (svc *Service) Comment(cRequest controller.CommentRequest) (string, error) {
+func (svc *Service) CommentAction(cRequest controller.CommentRequest) (string, error) {
 	if cRequest.ActionType == 1 {
 		return svc.d.CommentAdd(cRequest)
 	}
 	return svc.d.CommentDelete(cRequest)
+}
+func (svc *Service) CommentList(cRequest controller.CommentRequest) ([]model.Comment, error) {
+	videoId := cRequest.VideoId
+	return svc.d.CommentList(videoId)
 }
